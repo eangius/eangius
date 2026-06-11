8d30a7086a89cdfe1bf9f1dbe578d247a442cc1b
eangius <eliangius@hotmail.com>
2026-06-11 10:37:27 -0400

    links refactor

---
+++ src/data/links.ts
@@ -0,0 +1,5 @@
+// ABOUT: external reusable links
+export const githubUrl = "https://github.com/eangius";
+export const linkedinUrl = "https://www.linkedin.com/in/eliangius";
+export const mediumUrl = "https://medium.com/@eliangius";
+export const meetingUrl = "https://calendar.app.google/4jzAQbSYq3mzCsQa7";
